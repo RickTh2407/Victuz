3caa8decb5846f45e836e689c03e3140b7b244fb
RickTh <rick.thewissen@gmail.com>
2024-10-30 12:22:44 +0100

    30-10-2024

--- Victuz/obj/Debug/net8.0/Victuz.AssemblyInfo.cs
+++ Victuz/obj/Debug/net8.0/Victuz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Victuz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b89c0d62554332da357a2c753f2d4b705456079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df1fefc54a89d3005fbccc92742ce6d02f06db8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Victuz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Victuz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
