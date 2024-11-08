cfc141289cc43648921713108e15c1cfb8cc6ff4
SkiteV1 <msliekerwork@outlook.com>
2024-11-08 02:38:26 +0100

    Mike Night vibes

--- VictuzAPI/obj/Debug/net8.0/VictuzAPI.AssemblyInfo.cs
+++ VictuzAPI/obj/Debug/net8.0/VictuzAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VictuzAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2eb7c6178825dd3a2ad3509c213266c6c49d278")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff3b02a1656bb831140513cb728aae232bbe1b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VictuzAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VictuzAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
